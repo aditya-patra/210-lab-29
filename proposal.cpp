741828fd50e7b49edadf434919caa115e7f7fe19
Aditya Patra <adityabpatra@gmail.com>
2025-11-04 20:22:06 -0800

    pseudocode skeleton

---
+++ proposal.cpp
@@ -0,0 +1,10 @@
+// Necessary Headers: map, array, list, iostream, fstream(more may need to be added, but these are the basic ones)
+
+// Simulation function
+// function parameter: pointer to current map
+  // 
+
+// main function
+  // define map
+  // call simulation function
+  // repeat 60 times(300 years, 5 year interval)
