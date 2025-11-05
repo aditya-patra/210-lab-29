4da00992e2891058f089e088e180c9ac4bbbd520
Aditya Patra <adityabpatra@gmail.com>
2025-11-04 20:43:26 -0800

    pseudocode

--- proposal.cpp
+++ proposal.cpp
@@ -1,7 +1,7 @@
 // Necessary Headers: map, array, list, iostream, fstream(more may need to be added, but these are the basic ones)
 
 // Simulation function
-// function parameter: pointer to current map
+// function parameter: pointer to current map, pointer to number of available land segments
   // Iterate over farmers
     // Iterate over livestock species
       // For each livestock in current species, compare rand to chance of natural death of species to determine whether the animal dies
@@ -11,7 +11,12 @@
       // Keep track of number of dead animals and subtract from population at the end
     // compare rand to change of plague, if plague decrease livestock count by large percentage
     // Increase each livestock species by species-specific growth factor, make sure it doesn't exceed max population
-  // 
+  // For each piece of available land, use rand to choose farmer it goes to
+    // Use rand to choose type of animal(if animal already in farm, add land to that list index)
+  // Iterate over farmers
+    // Recalculate value of each livestock species and update value list
+    // If value less than 25, erase that index from all lists in that farmer
+    // If total value of farmer is 0, erase farmer from map
   // - propogation: population increase is based on livestock cap and rate of growth varies by species
   // - natural death: chance of death varies between livestock species
   // - sudden illness: all livestock have equal chance of becoming ill
@@ -24,6 +29,7 @@
 
 // main function
   // instantiate map
+  // set number of available land segments
   // create file reader
   // read file to form map(each row should contain a)
   // call simulation function
