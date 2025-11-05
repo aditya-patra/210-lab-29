142ee71d86fd7269c88628a91ea0e3820476c1d5
Aditya Patra <adityabpatra@gmail.com>
2025-11-04 20:48:21 -0800

    pseudocode

--- proposal.cpp
+++ proposal.cpp
@@ -15,17 +15,8 @@
     // Use rand to choose type of animal(if animal already in farm, add land to that list index)
   // Iterate over farmers
     // Recalculate value of each livestock species and update value list
-    // If value less than 25, erase that index from all lists in that farmer
+    // If value less than 25, store index in temp variable, check temp next iteration, iterate through each list and delete index
     // If total value of farmer is 0, erase farmer from map
-  // - propogation: population increase is based on livestock cap and rate of growth varies by species
-  // - natural death: chance of death varies between livestock species
-  // - sudden illness: all livestock have equal chance of becoming ill
-  // - plague: farmer loses most of a livestock species
-  // After livestock change, program determines changes in land. If there is available land, it is randomly split between existing farmers
-  // - when land is given to a farmer, there is an equal chance of it going to any livestock species or going to a new species
-  // Value of each livestock for each farmer is then calculated.
-  // If value of any livestock species is less than 25, farmer sells corresponding land as rent is more than earnings.
-  // If a farmer loses all of his land, he goes bankrupt and is no longer included
 
 // main function
   // instantiate map
