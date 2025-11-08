973c1006d738b0ab7c029f94eb9b089d564f28d2
Aditya Patra <adityabpatra@gmail.com>
2025-11-07 18:52:42 -0800

    finished input

--- simulation.cpp
+++ simulation.cpp
@@ -9,20 +9,23 @@ using namespace std;
 
 int main () {
     ifstream file("farmers.txt");
-    map<string, array<list<string>, 3>>;
+    map<string, array<list<string>, 3>> farmers;
     string reader;
     while(getline(file, reader)) {
+        array<list<string>, 3> temp;
         string name = reader;
-        getline(file, reader);
-        string animal1 = reader;
-        getline(file, reader);
-        string count1 = reader;
-        getline(file, reader);
-        string plot1 = reader;
-        array<list<string>, 3> temp1;
-        temp1[0].push_back(animal1);
-        temp1[0].push_back(count1);
-        temp1[0].push_back(plot1);
+        for(int i = 0; i < 3; i++) {
+            getline(file, reader);
+            string animal1 = reader;
+            getline(file, reader);
+            string count1 = reader;
+            getline(file, reader);
+            string plot1 = reader;
+            temp[i].push_back(animal1);
+            temp[i].push_back(count1);
+            temp[i].push_back(plot1);
+        }
+        farmers.insert(make_pair(name, temp));
     }
     return 0;
 }
