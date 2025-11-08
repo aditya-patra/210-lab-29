5adbba97fc7e14397152b474cdaf773883d6d190
Aditya Patra <adityabpatra@gmail.com>
2025-11-07 18:56:10 -0800

    writing input test code

--- simulation.cpp
+++ simulation.cpp
@@ -30,7 +30,13 @@ int main () {
     for (const auto& pair : farmers){
         cout << pair.first << ": "  << endl;
         for (int i = 0; i < 3; i++) {
-            
+            int e = 0;
+            for (auto it = pair.second[0].begin(); it != pair.second[0].end(); ++it) {
+                if (e == i) {
+                    cout << *it << " ";
+                }
+                e++;
+            }
         }
     }
     return 0;
