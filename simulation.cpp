20188dbfc25d2f12252883ae9733b3d06d9a5718
Aditya Patra <adityabpatra@gmail.com>
2025-11-07 18:50:50 -0800

    continuing file input

--- simulation.cpp
+++ simulation.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main () {
-    ifstream file("farmerss.txt");
+    ifstream file("farmers.txt");
     map<string, array<list<string>, 3>>;
     string reader;
     while(getline(file, reader)) {
@@ -19,6 +19,10 @@ int main () {
         string count1 = reader;
         getline(file, reader);
         string plot1 = reader;
+        array<list<string>, 3> temp1;
+        temp1[0].push_back(animal1);
+        temp1[0].push_back(count1);
+        temp1[0].push_back(plot1);
     }
     return 0;
 }
