b0e44c5662080a2f86f05ee0f52ba4a313fe8513
Aditya Patra <adityabpatra@gmail.com>
2025-11-07 18:46:34 -0800

    trying to get file input

--- simulation.cpp
+++ simulation.cpp
@@ -3,6 +3,22 @@
 #include <list>
 #include <iostream>
 #include <fstream>
+#include <string>
+
+using namespace std;
+
 int main () {
+    ifstream file("farmers.txt");
+    map<string, array<list<string>, 3>>;
+    string reader;
+    while(getline(file, reader)) {
+        string name = reader;
+        getline(file, reader);
+        string animal1 = reader;
+        getline(file, reader);
+        string count1 = reader;
+        getline(file, reader);
+        string plot1 = reader;
+    }
     return 0;
 }
