852b3f40cdf2d1a539bc226e0d7bced06cf0cf82
Aditya Patra <adityabpatra@gmail.com>
2025-11-09 10:24:21 -0800

    cleaning up code

--- simulation.cpp
+++ simulation.cpp
@@ -7,11 +7,12 @@
 #include <vector>
 
 using namespace std;
-const int RAND_CHANCE_DEATH = 10;
-const int RAND_CHANCE_ILLNESS = 50;
-const int RAND_CHANCE_PLAGUE = 6;
+const int RAND_CHANCE_DEATH = 6;
+const int RAND_CHANCE_ILLNESS = 20;
+const int RAND_CHANCE_PLAGUE = 3;
 const int TOTAL_PLOTS = 1110;
-const int max_pop = 120;
+const int MAX_POP = 120;
+const int BIRTH_RATE = 6;
 
 // function converts input string to interger
 int string_to_int(string val, map<char, int> counts) {
@@ -93,9 +94,9 @@ void simulation(map<string, array<list<string>, 3>>& farmers, map<char, int> cou
                 curr_pop *= (((double)(rand() % 11)+10)/100.0);
             }
             // get number of births
-            curr_pop += curr_pop * 10 / animal_value;
-            if (curr_pop > max_pop / animal_value) {
-                curr_pop = max_pop / animal_value;
+            curr_pop += curr_pop * BIRTH_RATE / animal_value;
+            if (curr_pop > MAX_POP / animal_value) {
+                curr_pop = MAX_POP / animal_value;
             }
             // save new population
             new_pops.push_back(curr_pop);
@@ -145,7 +146,6 @@ void simulation(map<string, array<list<string>, 3>>& farmers, map<char, int> cou
         }
     }
     for(string val: farmers_to_delete) {
-        cout << "Erasing farmer " << val << endl;
         farmers.erase(val);
     }
     // update plot allocation
@@ -161,7 +161,6 @@ void simulation(map<string, array<list<string>, 3>>& farmers, map<char, int> cou
             }
             e++;
         }
-        // cout << farmer_name << endl;
         // find animal to add
         int rand_animal = (int)(rand() % 5);
         string animal;
@@ -254,8 +253,7 @@ int main () {
     map<int, string> counts_rev = {{0, "0"}, {1, "1"}, {2, "2"}, {3, "3"}, {4, "4"}, {5, "5"}, {6, "6"}, {7, "7"}, {8, "8"}, {9, "9"}};
     map<string, int> animal_val = {{"Cow", 5}, {"Horse", 6}, {"Sheep", 4}, {"Chicken", 1}, {"Pig", 3}};
     // file input
-    int i = 0;
-    while(getline(file, reader) && i < 10) {
+    while(getline(file, reader)) {
         // getting each farmer at once(each list in the array will contain one set of values, farmer animals, animal counts, or allocated fields)
         array<list<string>, 3> temp;
         string name = reader;
@@ -289,13 +287,12 @@ int main () {
         temp[2].push_back(plot3);
         // create new index in farmer map
         farmers.insert(make_pair(name, temp));
-        i++;
     }
-    driver_function(farmers, counts, counts_rev, animal_val);
+    // driver_function(farmers, counts, counts_rev, animal_val);
 
     // simulation run
-    /*
     for(int i = 0; i <= 300; i += 5) {
+        cout << "Farmer List ---------------------------------------------------" << endl;
         for (const auto& pair : farmers){
             cout << pair.first << ": ";
             for (int i = 0; i < 3; i++) {
@@ -329,38 +326,5 @@ int main () {
         // run simulation
         simulation(farmers, counts, counts_rev, animal_val);
     }
-    */
-    /*
-    for (const auto& pair : farmers){
-        cout << pair.first << ": ";
-        for (int i = 0; i < 3; i++) {
-            int e = 0;
-            for (auto it = pair.second[0].begin(); it != pair.second[0].end(); ++it) {
-                if (e == i) {
-                    cout << *it << " ";
-                    break;
-                }
-                e++;
-            }
-            e = 0;
-            for (auto it = pair.second[1].begin(); it != pair.second[1].end(); ++it) {
-                if (e == i) {
-                    cout << *it << " ";
-                    break;
-                }
-                e++;
-            }
-            e = 0;
-            for (auto it = pair.second[2].begin(); it != pair.second[2].end(); ++it) {
-                if (e == i) {
-                    cout << *it << " ";
-                    break;
-                }
-                e++;
-            }
-        }
-        cout << endl;
-    }
-    */
     return 0;
 }
